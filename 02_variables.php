308f3ed0abf01edbab3fda31a1886be872aba758
Lasya <lasyac07@gmail.com>
2026-06-29 14:36:02 +0530

    variables rules and syntax in php

---
+++ 02_variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = 'Lasya';//string
+$age=20;//int
+$has_kids=true;//boolean
+$cash_on_hand=20.75;//float
+
+
+// var_dump ($cash_on_hand);
+// echo $name . ' is ' . $age . ' years old';
+
+// echo "$name is $age years old";  
+$x= '5'+'5';
+var_dump($x);
+echo 10-5;
+echo 10*5;
+echo 10%5;
